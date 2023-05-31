3f1876bd5578810655328754a1be336264539c40
DESKTOP-R684JVL\ryuna <ryunad@gmail.com>
2023-05-31 18:14:19 +0900

    # Modifier를 사용해서 레이아웃 수정 - 이미지 추가

--- app/src/main/java/com/example/jiwanicomposestudy/MainActivity.kt
+++ app/src/main/java/com/example/jiwanicomposestudy/MainActivity.kt
@@ -3,16 +3,26 @@ package com.example.jiwanicomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jiwanicomposestudy.ui.theme.JiwaniComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,8 +34,7 @@ class MainActivity : ComponentActivity() {
             JiwaniComposeStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
@@ -44,8 +53,7 @@ fun MessageCard(name: String) {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello $name!", modifier = modifier
     )
 }
 
@@ -65,7 +73,7 @@ fun MessageCardPreviewed() {
     MessageCard(Message("jiwan", "study"))
 }
 
-//region layout
+//region Layout + Image
 
 data class Message(val author: String, val body: String)
 
@@ -74,11 +82,25 @@ fun MessageCard(msg: Message) {
     // Row {} 수평으로 정렬
     // Box {} 같은 위치에 쌓임
     // Column {} 수직으로 정렬
-    Column {
-        Text(text = msg.author)
-        Text(msg.body)
+
+    // Modifier : 각 요소들의 상태를 수정할 수 있다. 크기나 간격 등...
+    Row(Modifier.padding (all = 8.dp)) {
+        Image(
+            painter = painterResource(
+            id = R.drawable.linus),
+            contentDescription = "profile",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(msg.body)
+        }
     }
 }
-
 
 //endregion
