ddb43d563cc2515367e8ad0ac8c71fba97c644a0
DESKTOP-R684JVL\ryuna <ryunad@gmail.com>
2023-06-13 03:17:48 +0900

    # MaterialTheme의 shape, color, size fontStyle등등 지정방법

--- app/src/main/java/com/example/jiwanicomposestudy/steps/theme/ui/Home.kt
+++ app/src/main/java/com/example/jiwanicomposestudy/steps/theme/ui/Home.kt
@@ -2,38 +2,54 @@ package com.example.jiwanicomposestudy.steps.theme.ui
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.ListItem
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Palette
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jiwanicomposestudy.R
@@ -94,14 +110,24 @@ fun Header(
     text: String,
     modifier: Modifier = Modifier
 ) {
-    Text(
-        text = text,
+    Surface(
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
+        contentColor = MaterialTheme.colors.primary,
         modifier = modifier
-            .fillMaxWidth()
-            .background(Color.LightGray)
-            .semantics { heading() }
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-    )
+    ) {
+        Text(
+            text = text,
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun HeaderPreview() {
+    Header("Hello Material", Modifier.height(58.dp))
 }
 
 @Composable
@@ -120,16 +146,21 @@ fun FeaturedPost(
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
+                    /*.clip(shape = MaterialTheme.shapes.small)*/
                     .heightIn(min = 180.dp)
                     .fillMaxWidth()
             )
             Spacer(Modifier.height(16.dp))
 
             val padding = Modifier.padding(horizontal = 16.dp)
-            Text(
-                text = post.title,
-                modifier = padding
-            )
+
+            // 특정 텍스트 스타일 지정하는 함수
+            ProvideTextStyle(value = TextStyle(fontWeight = FontWeight.Bold, fontFamily = FontFamily.Monospace)) {
+                Text(
+                    text = post.title,
+                    modifier = padding
+                )
+            }
             Text(
                 text = post.metadata.author.name,
                 modifier = padding
@@ -147,6 +178,9 @@ private fun PostMetadata(
 ) {
     val divider = "  •  "
     val tagDivider = "  "
+    val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+        background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+    )
     val text = buildAnnotatedString {
         append(post.metadata.date)
         append(divider)
@@ -156,13 +190,22 @@ private fun PostMetadata(
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.uppercase(Locale.getDefault())} ")
+            withStyle(tagStyle) {
+                append(" ${tag.uppercase()} ")
+            }
         }
     }
-    Text(
-        text = text,
-        modifier = modifier
-    )
+
+
+
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Surface {
+            Text(
+                text = text,
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -206,6 +249,13 @@ private fun FeaturedPostPreview() {
     JetnewsTheme {
         FeaturedPost(post = post)
     }
+}
+
+@Composable
+fun FloatingActionButton(
+    backgroundColor: Color = MaterialTheme.colors.secondary
+) {
+
 }
 
 @Preview("Home")
