6c84cb50c3b44c0baf10654757d1b357a003dd9e
DESKTOP-R684JVL\ryuna <ryunad@gmail.com>
2023-06-14 01:10:21 +0900

    # animateContentSize를 사용해서 상태에 따른 확장 뷰로 변환

--- app/src/main/java/com/example/jiwanicomposestudy/steps/animations/AnimateAsState.kt
+++ app/src/main/java/com/example/jiwanicomposestudy/steps/animations/AnimateAsState.kt
@@ -2,6 +2,7 @@ package com.example.jiwanicomposestudy.steps.animations
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
@@ -13,6 +14,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,8 +27,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -40,6 +44,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -51,9 +56,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jiwanicomposestudy.R
@@ -67,11 +75,14 @@ enum class TabPage {
 
 @Composable
 fun animateAsState() {
+    val allTopics = stringArrayResource(id = R.array.topics).toList()
+
     var tabPage by remember { mutableStateOf(TabPage.Home) }
     val lazyListState = rememberLazyListState()
 
-    var weatherLoading by remember { mutableStateOf(TabPage.Home) }
+    val weatherLoading by remember { mutableStateOf(false) }
     var editMessageShown by remember { mutableStateOf(false) }
+    var expandedTopic by remember { mutableStateOf<String?>(null) }
     val coroutineScope = rememberCoroutineScope()
 
     suspend fun showEditMessage() {
@@ -110,16 +121,30 @@ fun animateAsState() {
             state = lazyListState,
             modifier = Modifier.padding(padding)
         ) {
-            /*item { Header(title = stringResource(R.string.weather)) }
+            item { Header(title = stringResource(R.string.weather)) }
             item { Spacer(modifier = Modifier.height(16.dp)) }
             item {
                 Surface(
                     modifier = Modifier.fillMaxWidth(),
                     elevation = 2.dp
                 ) {
+                    if (weatherLoading) {
 
+                    }
                 }
-            }*/
+            }
+
+            item { Header(title = stringResource(id = R.string.topics)) }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            items(allTopics) { topic ->
+                TopicRow(
+                    topic = topic,
+                    expanded = expandedTopic == topic,
+                    onClick = {
+                        expandedTopic = if (expandedTopic == topic) null else topic
+                    }
+                )
+            }
         }
 
         EditMessage(shown = editMessageShown)
@@ -149,18 +174,72 @@ private fun EditMessage(shown: Boolean) {
             Text(
                 text = stringResource(id = R.string.edit_message),
                 modifier = Modifier.padding(16.dp)
-            ) 
-            
-            
+            )
         }
     }
 }
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun TopicRow(topic: String, expanded: Boolean, onClick: () -> Unit) {
+    TopicRowSpacer(visible = expanded)
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        elevation = 2.dp,
+        onClick = onClick
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .animateContentSize()
+        ) {
+            Row {
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(
+                    text = topic,
+                    style = MaterialTheme.typography.body1
+                )
+            }
+
+            if (expanded) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(id = R.string.lorem_ipsum),
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+    TopicRowSpacer(visible = expanded)
+}
+
+@Composable
+fun TopicRowSpacer(visible: Boolean) {
+    AnimatedVisibility(visible = visible) {
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTopicSpacer() {
+    TopicRowSpacer(visible = true)
+}
+
 
 @Composable
 fun Header(
     title: String
 ) {
-    Text(text = title,
+    Text(
+        text = title,
         modifier = Modifier.semantics { heading() },
         style = MaterialTheme.typography.h5
     )
@@ -288,11 +367,11 @@ fun HomeFloatingActionButton(
     }
 }
 
-@Preview 
+@Preview
 @Composable
 private fun PreviewHomeFloatingActionButton() {
     HomeFloatingActionButton(extended = true) {
-        
+
     }
 }
 
